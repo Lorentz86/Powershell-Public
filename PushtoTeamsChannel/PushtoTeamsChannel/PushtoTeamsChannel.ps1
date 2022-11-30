9593fee31e3a3797041ca79374535ff634849e8b
Lorentz <84975855+Lorentz86@users.noreply.github.com>
2022-11-30 15:06:47 +0100

    Add files via upload

---
+++ PushtoTeamsChannel/PushtoTeamsChannel/PushtoTeamsChannel.ps1
@@ -0,0 +1,94 @@
+<#
+  .SYNOPSIS
+  Send a message to a teams channel using a webhook. 
+
+  .DESCRIPTION
+  You can import this script using Import-module [path of script].ps1. 
+  Link how to make a webhook in a teams chanel: 
+  https://docs.microsoft.com/en-us/microsoftteams/platform/webhooks-and-connectors/how-to/add-incoming-webhook 
+  Paste the link in the [Process] part of the script. 
+
+  .PARAMETER Channel
+  Specifies the channel where the message should be in. 
+
+  .PARAMETER Title
+   Specifies the Title of the message
+
+   .PARAMETER Message
+   Specifies the content of the message
+
+  .INPUTS
+  None. You cannot pipe objects to PushtoTeamschannel.ps1.
+
+  .OUTPUTS
+  If you run this script in ISE you will reciece a "1" if the message has been delivered. 
+
+  .EXAMPLE
+  PS> Push-Message -Channel "Update" -Title "Example" -Message "This is an example"  
+
+  .EXAMPLE
+  PS> Push-Message -Channel "Security" -Title "Example" -Message "This is an example of a security warning"  
+
+  .EXAMPLE with variable
+  PS> Push-Message -Channel "Security" -Title $Title -Message $Message  
+
+#>
+
+function Push-Message 
+{
+	[cmdletbinding()]
+	param 
+	(
+		[Parameter(Mandatory=$true)]  
+		[ValidateSet("Update","Error","Security")] # Adjust Channel names that you use. I have set up 3 channels, so add or remove if needed. 
+		[string]
+		$Channel,
+
+		[Parameter(Mandatory=$true)]  #Title parameter
+		[string]
+		$Titel,
+
+		[Parameter(Mandatory=$true)]  #Message parameter
+		[string]
+		$Message
+	)
+
+	$Update = 'Update'
+	$Errors = 'Error'
+	$Security = 'Security'
+									#$JSONBody contains the payload of the message.  
+	$JSONBody = [PSCustomObject][Ordered]@{
+	"@type" = "MessageCard"
+	"@context" = "<http://schema.org/extensions>"
+	"summary" = "$Channel"
+	"themeColor" = '0078D7'
+	"title" = "$Title"}
+
+
+
+
+	if ($Update,$Errors,$Security -notcontains $Channel)
+	{
+		Write-host "Please input a channel The following have been configuered: $Update, $Error or $Security"
+	}
+	elseif ($Channel -eq $Update)
+	{
+		$webhook = "https://yourdomain.webhook.office.com"
+	}
+	elseif ($Channel -eq $Error) 
+	{
+		$webhook = "https://yourdomain.webhook.office.com"
+	}
+	elseif ($Channel -eq $Security) 
+	{
+		$webhook = "https://yourdomain.webhook.office.com"
+	}
+	
+	$Body = ConvertTo-Json $JSONBody
+
+	End
+	{
+		Invoke-RestMethod -uri $webhook -Method Post -body $Body -ContentType 'application/json' # This command will send a message to the webhook.
+
+	}
+}
---
+++ SQL/SQL_Powershell_DeleteRows/SQL_Powershell_DeleteRows/SQL_Powershell_DeleteRows.ps1
@@ -0,0 +1,60 @@
+﻿# PS Modules requiered: sqlserver
+# Tested with SQL server 2014 and PS 5.1
+# 
+
+<# 
+# To see the month use this query
+$srv = "SQLsrv"
+$testmonth = -52
+$querydate = "Select DATEADD(Month, $testmonth, GETDATE()) as date"
+Invoke-Sqlcmd -ServerInstance $srv -Query $querydate
+
+#>
+
+#SQL Server Info
+$srv = "SQLsrv"
+$DB = "DBNAME
+
+# Log Cleaning after deletion of rows.
+$querylog = "DECLARE @DbName VARCHAR(25)
+DECLARE @DbLog VARCHAR(25)
+SET @DbName = DB_NAME()
+SET @DbLog = FILE_NAME(2)
+EXEC('ALTER DATABASE ' + @DbName + ' SET RECOVERY SIMPLE')
+DBCC SHRINKFILE(@DbLog,2)
+"
+
+# Copy from here and past it with a diffirent Table name to Run the loop for a diffirent table after this one is done.
+# COPY START
+
+# Name of table where you want to delete rows. 
+$table = 'Tablename'
+
+# FilterObject / Rowname
+$date = 'nameofRow'
+
+
+# Target month, end target is $endTarget months before todays date. 
+
+$month = -52
+$endTarget = -13
+
+# Start of loop
+
+for($month; $month -le $endTarget; $month ++) {
+
+$Runs = ($month - $endTarget ) * -1
+
+Write-Host "Starting deleting of $table where month is $month from todays month. There are $Runs runs left "}
+
+$query = "Delete from $table where $date < DATEADD(Month, $month, GETDATE())"
+
+Invoke-Sqlcmd -ServerInstance $srv -Database $DB -Query $query
+
+Invoke-Sqlcmd -ServerInstance $srv -Database $DB -Query $querylog
+
+}
+
+Write-host "The script is done for $table" 
+
+# COPY END
