0033551c8bb38fe84f52feaaf6610b02919cc8a4
Lorentz <84975855+Lorentz86@users.noreply.github.com>
2022-11-30 15:10:55 +0100

    Revert "commit"
    This reverts commit eb306410712d9ea514766270385592c473377748.

---
+++ PrepareVHDtoAzure/checkservices.ps1
@@ -0,0 +1,7 @@
+Get-Service -Name BFE, Dhcp, Dnscache, IKEEXT, iphlpsvc, nsi, mpssvc, RemoteRegistry |
+  Where-Object StartType -ne Automatic |
+    Set-Service -StartupType Automatic
+
+Get-Service -Name Netlogon, Netman, TermService |
+  Where-Object StartType -ne Manual |
+    Set-Service -StartupType Manual
---
+++ PrepareVHDtoAzure/enviromentalVariable.ps1
@@ -0,0 +1,2 @@
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' -Name TEMP -Value "%SystemRoot%\TEMP" -Type ExpandString -Force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' -Name TMP -Value "%SystemRoot%\TEMP" -Type ExpandString -Force
