4a2fc40a415e34900e67443ebfa39b667b104b09
Lorentz <84975855+Lorentz86@users.noreply.github.com>
2023-06-13 11:43:27 +0200

    New Script to move vms to preferred servers

---
+++ WindowsServer/Hyper-V/MoveVMtoPreferredNode.ps1
@@ -0,0 +1,25 @@
+﻿#Script To move VMS to their preferred nodes. 
+# PS Modules Needed FailoverClusters.
+# Make a csv file containing VM's and their preferred nodes. Example will be uploaded
+$prefNodes = Import-csv "Path to File"
+
+
+$AllFailOverVMs = Get-ClusterResource | Where-Object -Property ResourceType -EQ "Virtual Machine"
+
+foreach($prefNode in $prefNodes)
+{
+    foreach($VM in $AllFailOverVMs)
+    {
+        $vmname = $VM.Name.Replace("Virtual Machine ","")
+        
+        if($vmname.Equals($prefNode.'Virtual Machine'))
+        {
+            # the ! reverts the output of equals. 
+            if(!$VM.OwnerNode.Name.Equals($prefNode.'Preferred Node'))
+            {
+                Write-Host "Moving $vmname to $($prefNode.'Preferred Node')"
+                Move-ClusterVirtualMachineRole -Name $vmname -Node $prefNode.'Preferred Node' -Verbose
+            }
+        }
+    }
+}
