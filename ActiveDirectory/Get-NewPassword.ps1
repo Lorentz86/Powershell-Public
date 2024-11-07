7b07c986d6656045ab5f0041d66add11f138e898
Lorentz <84975855+Lorentz86@users.noreply.github.com>
2024-11-07 14:03:06 +0100

    Now the random options work better

--- ActiveDirectory/Get-NewPassword.ps1
+++ ActiveDirectory/Get-NewPassword.ps1
@@ -17,24 +17,44 @@ function Get-NewPassword {
     $Url = "https://password.ninja/api/password?"
     $MinLength = "minPassLength=" + $Length
 
-    $RandomArray = @{
-        Animals = "&animals=true"
-        Colour = "&colours=true"
-        Food = "&food=true"
-        Instruments = "&Instruments=true"
-        shapes = "&shapes=true"
-        sports = "&sports=true"
-        transport = "&transport=true"
-    }
-
     try {
-        $randomValueArray = $RandomArray.Values | Get-Random -Count 2
-        $randomValueString = $randomValueArray[0] + $randomValueArray[1]
+        # Define the options
+        $Options = @{
+            Animals = "&animals="
+            Colour = "&colours="
+            Food = "&food="
+            Instruments = "&Instruments="
+            Shapes = "&shapes="
+            Sports = "&sports="
+            Transport = "&transport="
+        }
 
+        # Randomly select two options to be true
+        $TrueOptions = $Options.Keys | Get-Random -Count 2
+
+        # Randomize the order of the options
+        $RandomizedOptions = $Options.GetEnumerator() | Sort-Object { Get-Random }
+
+        # Build the options string
+        $OptionsString = ""
+        foreach ($option in $RandomizedOptions) {
+            if ($TrueOptions -contains $option.Key) {
+                $OptionsString += $option.Value + "true"
+            } else {
+                $OptionsString += $option.Value + "false"
+            }
+        }
+
+        # Extra options
         $Symbols = "&symbols=true"
         $CapitalLetter = "&capitals=true"
-        $TotalUrl = $Url + $MinLength + $randomValueString + $CapitalLetter + $Symbols
+        $NumAtEnd = "&numAtEnd=3"
+        $ExcludeSymbols = "&excludeSymbols=pf"
 
+        # Generate the URL
+        $TotalUrl = $Url + $MinLength + $OptionsString + $CapitalLetter + $Symbols + $NumAtEnd + $ExcludeSymbols
+
+        # Retrieve the password
         $Password = Invoke-RestMethod -Method Get -Uri $TotalUrl
         return $Password
     }
